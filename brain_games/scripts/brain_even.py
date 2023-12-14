be561f3a4d711ff9ae1b0d061b882b5180dc249b
tve1n <timarumak@gmail.com>
2023-12-14 16:20:14 +0500

    end logic

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,48 +3,44 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string("May I have your name? ")
     print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
 
 
-def answer(checking_even, question):
-    fail = (" 'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, Bill!")
-    if checking_even == 0 and question == 'yes':  # Число четное и угадал
-        return "Correct!"
-    elif checking_even == 0 and question == 'no':  # Число четное и не угадал
+def answer(num):
+    checking_even = num % 2  # 1 - нечетное 0 - четное
+    t_again = "Let's try again, " + name + "!"
+    fail = "'yes' is wrong answer ;(. Correct answer was 'no'. \n" + t_again
+    f_use = question()
+    if checking_even == 0 and f_use == 'yes':  # Число четное и угадал
+        return cor
+    elif checking_even == 0 and f_use == 'no':  # Число четное и не угадал
         return fail
-    elif checking_even == 1 and  question == 'no':  # Число нечетное и угадал
-        return "Correct"
-    elif checking_even == 1 and  question == 'yes':  # Число нечетное и не угадал
+    elif checking_even == 1 and  f_use == 'no':  # Число нечетное и угадал
+        return cor
+    elif checking_even == 1 and  f_use == 'yes':  # Число нечетное и не угадал
         return fail
-    #return checking_even != True
+    else:
+        return fail
 
 
 def question():
-    que = prompt.string("Your answer: ") # Ответ от пользователя
+    que = prompt.string("Your answer: ")  # Ответ от пользователя
     return que
 
 
-n = randint(1, 101)
-checking_even = n % 2  # 1 - нечетное 0 - четное
+def loop_3times():
+    for i in range(3):
+        n = randint(1, 100)
+        print('Question: ' + str(n))
+        kri = answer(n)
+        print(kri)
+        if kri != cor:
+            break
+    print(f"Congratulations, {name}!")
 
 
+cor = "Correct!"
+name = prompt.string("May I have your name? ")
 welcome_user()
-print(n, checking_even)
-print(answer(checking_even, question()))
-
-
-
-
-"""
-ЗАДАЧИ:
-1) Рандомное число(import random)
-2) Проверка числа на четность/нечетность
-3) Цикл на 3 подряд вопроса
-3.1) не правильный, прервать цикл break
-
-Для начала сделать с одним числом
-0 = False 
-1 = True
-"""
+loop_3times()
