abe420355a43934c6345f09fad81b6573021b722
tve1n <timarumak@gmail.com>
2024-01-21 23:03:03 +0500

    reorganization

---
+++ brain_games/games/calc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from random import randint, choice
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+def calculation(n1, znak, n2):
+    if znak == '+':
+        return n1 + n2
+    elif znak == "-":
+        return n1 - n2
+    elif znak == '*':
+        return n1 * n2
+
+
+
+rules = "What is the result of the expression?"
+
+def randss():
+    spisok = ['+', "-", "*"]
+    sp4 = [randint(1, 100), randint(1, 100)]
+    n1, n2 = max(sp4), min(sp4)
+    znak = choice(spisok)
+    OP = f'{n1} {znak} {n2}'
+    TA = calculation(n1, znak, n2)
+    return OP, TA
--- brain_games/scripts/game_logic.py
+++ brain_games/scripts/game_logic.py
@@ -1,38 +1,22 @@
 import prompt
 
 
-def welcome_user(game):  # Функция для приветствия
-    print("Welcome to the Brain Games!")
-
-    name_u = prompt.string("May I have your name? ")
-
+def game_engine(game): 
+    print("Welcome to the Brain Games!") 
+    name_u = prompt.string("May I have your name? ")  
     print(f'Hello, {name_u}!')
-    print(game)
-    return name_u
-
-
-def answer():  # Функция для запроса ответа
-    que = prompt.string("Your answer: ")
-    return que
-
-
-def reaction(name_u, answer1, answer2):  # Функция для реакции
-    t_again = f"Let's try again, {name_u}!"
-    fail = f"'{answer1}' is wrong answer ;(. Correct answer was '{answer2}'."
-    return f"{fail}\n{t_again}"
-
-
-def check_answer(otvet, otv_user):
-    if otvet == otv_user:
-        return True
+    print(game.rules)  # 1) Правила игры 
+    for i in range(3):
+        operation, true_answer = game.randss()  # две переменные разом
+        print(f'Question: {operation}')  # 2) ПЕРЕМЕННАЯ!!
+        guess = int(prompt.string("Your answer: "))
+        t_again = f"Let's try again, {name_u}!"
+        fail = f"'{guess}' is wrong answer ;(. Correct answer was '{true_answer}'."
+        if true_answer == guess:  # 3) ПЕРЕМЕННАЯ
+            print('Correct')
+            continue
+        else:
+            print(f'{fail}\n{t_again}')
+            break
     else:
-        return False
-
-
-def game_engine(game):
-    pass
-'''
-1) все выводы на экран
-2) запрос имени пользователя и ответа
-3) сикл с раундами
-'''
+        print(f"Congratulations, {name_u}!")
